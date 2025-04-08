3d9a70dee94be8b11125c7928655400479007864
arcm777 <arcmaharshi2105@gmail.com>
2025-04-08 13:51:37 +0000

    Add djongo database engine configuration to settings.py

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = "octofit_tracker.wsgi.application"
 
 DATABASES = {
     'default': {
-        'ENGINE': 'djongo',
+        'ENGINE': 'djongodb',
         'NAME': 'octofit_db',
         'HOST': 'localhost',
         'PORT': 27017,
